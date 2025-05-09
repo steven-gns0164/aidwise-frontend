f99779fcb96ce836be8022489646f3bec2d92137
peter <peter_krn@hotmail.com>
2025-05-09 20:30:57 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'aidwise-frontend',
+  webDir: 'dist'
+};
+
+export default config;
